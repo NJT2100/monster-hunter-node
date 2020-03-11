6222fe31a40b38a77972284d7158b63dbe1668ac
Nathan Thomas <njordanthomas@gmail.com>
2020-03-10 22:06:34 -0400

    updated panels

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,17 @@ router.get('/', function(req, res) {
         message: 'Welcome to the Monter Hunter Wiki',
         poppages: ['item', 'item', 'item', 'item', 'item', 'item', 'item', 'item', 'item', 'item', 'item', 'item'],
         featuredImages: ['featured_1.png', 'featured_2.jpg', 'featured_3.jpg', 'featured_4.jpg'],
-        gameImages: ['mhw-iceborne.png', 'mhw.png', 'mhxx.png'],
+        gameImages: [
+            {'image':'mhw-iceborne.png', 'title':'Monster Hunter World: Iceborne'}, 
+            {'image':'mhw.png', 'title':'Monster Hunter World'},
+            {'image':'mhxx.png', 'title':'Monster Hunter Generations Ultimate'}],
+        mainContentImages: [
+            {'image':'1.png', 'text':'Weapons'},
+            {'image':'2.png', 'text':'Armor'},
+            {'image':'3.png', 'text':'Monsters'},
+            {'image':'4.png', 'text':'Quests'},
+            {'image':'5.png', 'text':'Items'},
+            {'image':'6.png', 'text':'Locations'}],
     });
 });
 
